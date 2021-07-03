e463fb3901354121e4c408e2eb4cc90aba9942bc
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-07-03 12:27:28 -0700

    setup resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,6 +2,43 @@ import React from 'react';
 
 export default function Resume() {
     return (
-        <div>Resume</div>
+        <div className="resume">
+            <h1>Resume</h1>
+            <div>
+                <h2>Front-End</h2>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>Jquery</li>
+                    <li>Bootstrap</li>
+                    <li>Pure CSS</li>
+                    <li>React</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Back-End</h2>
+                <ul>
+                    <li>APIs</li>
+                    <li>NodeJS</li>
+                    <li>Express</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>Mongoose</li>
+                    <li>Model View Controllers</li>
+                    <li>Progressive Web Applications</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Development</h2>
+                <ul>
+                    <li>NPM</li>
+                    <li>Git</li>
+                    <li>Jest</li>
+                    <li>Github</li>
+                    <li>Heroku</li>
+                </ul>
+            </div>
+        </div>
     )
 }
