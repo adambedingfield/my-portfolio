309e094a4cee0a6ecd3d9ff16d55de6e1f065c07
ACB <78452262+adambedingfield@users.noreply.github.com>
2021-07-03 11:56:05 -0700

    setup footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function() {
+    return (
+        <footer className="footer row">
+            <p className="col"><a href="https://github.com/adambedingfield">Github</a></p>
+            <p className="col"><a href="https://github.com/adambedingfield/my-portfolio">Repo</a></p>
+        </footer>
+    )
+}
